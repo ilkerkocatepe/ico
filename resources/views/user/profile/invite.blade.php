09009b42ccbd8f6639828cbe7b896f378dd433b7
İlker <kocatepeilker@gmail.com>
2021-01-31 23:08:00 +0300

    Invite & welcome page update

--- resources/views/user/profile/invite.blade.php
+++ resources/views/user/profile/invite.blade.php
@@ -32,8 +32,6 @@
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-sm-6">
                 <div class="card">
                     <div class="card-header mx-auto">
                         <h4 class="card-title">{{ __('Your invitation QR code') }}</h4>
@@ -41,14 +39,12 @@
                     <div class="card-body">
                         <div class="row">
                             <div class="col-xl-6 col-md-6 col-sm-6 col-12 pr-sm-0 mx-auto">
-                                {!! QrCode::size(200)->style('round',0.9)->errorCorrection('H')->generate(url('/register').'/'.$user->refer_hash); !!}
+                                {!! QrCode::size(200)->margin(100)->style('round',0.9)->errorCorrection('H')->generate(url('/register').'/'.$user->refer_hash); !!}
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="row">
             <div class="col-sm-6">
                 <div class="card">
                     <div class="card-header mx-auto">
@@ -59,31 +55,40 @@
                             <div class="table-responsive">
                                 <table class="table">
                                     <tbody>
-                                        <tr>
-                                            <td>{{__('System Status')}}</td>
-                                            <td>
-                                                @if(true)
-                                                    <span class="badge badge-light-success">{{__('Enabled')}}</span>
-                                                @else
-                                                    <span class="badge badge-light-warning">{{__('Disabled')}}</span>
-                                                @endif
-                                            </td>
-                                        </tr>
+                                    <tr>
+                                        <td>{{__('System Status')}}</td>
+                                        <td colspan="3" style="text-align: center;">
+                                            @if(\App\Models\Setting::value('mlm_status'))
+                                                <span class="badge badge-light-success">{{__('Enabled')}}</span>
+                                            @else
+                                                <span class="badge badge-light-warning">{{__('Disabled')}}</span>
+                                            @endif
+                                        </td>
+                                    </tr>
+                                    @if(\App\Models\Setting::value('mlm_status') && \App\Models\ReferenceLevel::all())
                                         <tr>
                                             <td>{{__('Level Limit')}}</td>
-                                            <td>5</td>
+                                            <td colspan="3" style="text-align: center;">{{ \App\Models\ReferenceLevel::all()->count() }}</td>
                                         </tr>
+                                        <tr>
+                                            <td>{{ __('Level') }}</td>
+                                            <td>{{ __('Minimum Balance') }}</td>
+                                            <td>{{ __('Maximum Earning') }}</td>
+                                            <td>{{ __('Rate') }}</td>
+                                        </tr>
+                                        @foreach(\App\Models\ReferenceLevel::all() as $level)
+                                            <tr>
+                                                <td>{{ __('Level') }} {{ $level->id }}</td>
+                                                <td>{{ $level->min_balance }}</td>
+                                                <td>{{ $level->max_earnings }}</td>
+                                                <td>{{ $level->rate }} %</td>
+                                            </tr>
+                                        @endforeach
+                                    @endif
                                     </tbody>
                                 </table>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-body">
-                        <img src="{{ asset('storage/assets/images/backend/mlm.png') }}" style="width: 100%; height: auto;" alt="Reference System" />
                     </div>
                 </div>
             </div>
