3008ce862b9427912f649c11842727e909082862
İlker <kocatepeilker@gmail.com>
2021-01-31 23:09:46 +0300

    ReferenceLevel Model

---
+++ app/Http/Controllers/ReferenceLevelController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ReferenceLevel;
+use Illuminate\Http\Request;
+
+class ReferenceLevelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ReferenceLevel  $referenceLevel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ReferenceLevel $referenceLevel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ReferenceLevel  $referenceLevel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ReferenceLevel $referenceLevel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ReferenceLevel  $referenceLevel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ReferenceLevel $referenceLevel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ReferenceLevel  $referenceLevel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ReferenceLevel $referenceLevel)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_01_31_194342_create_reference_levels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReferenceLevelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reference_levels', function (Blueprint $table) {
+            $table->id();
+            $table->integer('min_balance')->default(0);
+            $table->integer('max_earnings')->nullable();
+            $table->integer('rate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reference_levels');
+    }
+}
