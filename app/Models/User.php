c39c9f49f7ceecb1ae6f4ca69aa660dc1ec8f5b0
İlker <kocatepeilker@gmail.com>
2021-01-31 13:57:37 +0300

    User balance

--- app/Models/User.php
+++ app/Models/User.php
@@ -132,7 +132,7 @@ class User extends Authenticatable implements MustVerifyEmail, BannableContract
 
     public function balance()
     {
-        return $this->wallet()->balance;
+        return $this->wallet->balance;
     }
 
     public function status()
