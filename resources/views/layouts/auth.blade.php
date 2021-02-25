83821303e4bbd1396e2e52f12a40fa139a840518
ilkerkocatepe <kocatepeilker@gmail.com>
2021-02-26 01:10:27 +0300

    Auth Pages Update

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -6,8 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>@yield('title') - {{ \App\Models\Setting::where('setting','title')->first()->value }}</title>
-        <link rel="apple-touch-icon" href="{{asset('app-assets/images/ico/apple-icon-120.png')}}">
-        <link rel="shortcut icon" type="image/x-icon" href="{{asset('app-assets/images/ico/favicon.ico')}}">
+        <link rel="apple-touch-icon" href="{{ asset('storage/assets/front/images/logosq.png') }}">
+        <link rel="shortcut icon" href="{{ asset('storage/assets/front/images/logosq.png') }}" type="image/x-icon">
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/vendors.min.css')}}">
