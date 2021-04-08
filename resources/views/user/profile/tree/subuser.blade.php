fb2efe56b8a11bf07f6c23687c487d0ca62bc7cb
ilkerkocatepe <kocatepeilker@gmail.com>
2021-04-09 01:56:20 +0300

    tree edit

--- resources/views/user/profile/tree/subuser.blade.php
+++ resources/views/user/profile/tree/subuser.blade.php
@@ -2,7 +2,7 @@
 @foreach($childs as $child)
     @if($level<\App\Models\ReferenceLevel::all()->count()+1)
         <li class="tree-item">
-            <div class="tree-node">{{$child->name}} <br> LEVEL {{ $level }} <br> GAIN: {{ $user->referral_earning_amount($child->id) }} {{ env('TOKEN_SYMBOL') }}</div>
+            <div class="tree-node">{{$child->name}} <br> LEVEL {{ $level }} {{ $user->referral_earning_amount($child->id) ?? '<br> GAIN: '.$user->referral_earning_amount($child->id) . ' ' . env('TOKEN_SYMBOL')  }}</div>
             @if(count($child->children))
                 <ul class="tree-branch">
                     @include('user.profile.tree.subuser',['childs' => $child->children, 'level' => $level])
